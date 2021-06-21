04efda7ef49147330b901961df789a2b666e9f73
노경준 <developerjun0615@gmail.com>
2021-06-21 09:21:39 +0900

    [Update] UserRepository JPARepository 상속

--- src/main/java/com/server/token/repository/UserRepository.java
+++ src/main/java/com/server/token/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.server.token.repository;
 
-public class UserRepository {
+import com.server.token.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
 }
