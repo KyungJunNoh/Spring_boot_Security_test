5c326becc6aada6a5b1be8dee8141a35eaefd05a
노경준 <developerjun0615@gmail.com>
2021-06-21 09:12:47 +0900

    [Update] join 추가

--- src/main/java/com/server/token/controller/UserController.java
+++ src/main/java/com/server/token/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.server.token.controller;
 
+import com.server.token.domain.entity.User;
+import com.server.token.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public void join(@RequestBody User user){
+        userService.join(user);
+    }
 }
