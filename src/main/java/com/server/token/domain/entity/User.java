f1d2bfc388744dd2439d26e044266dd71c9fbac6
노경준 <developerjun0615@gmail.com>
2021-06-21 09:05:56 +0900

    [ADD] User

--- src/main/java/com/server/token/domain/entity/User.java
+++ src/main/java/com/server/token/domain/entity/User.java
@@ -1,4 +1,18 @@
 package com.server.token.domain.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
 public class User {
+    @Id @GeneratedValue
+    private Long userIdx;
+    private String userId;
+    private String userPw;
 }
