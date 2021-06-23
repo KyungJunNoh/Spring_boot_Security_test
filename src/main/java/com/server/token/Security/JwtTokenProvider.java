155b2a7d97ff5f2dd2384b49dec8d9902c30a2d6
노경준 <developerjun0615@gmail.com>
2021-06-23 16:21:09 +0900

    [Update] JwtTokenProvider 코드 추가

--- src/main/java/com/server/token/Security/JwtTokenProvider.java
+++ src/main/java/com/server/token/Security/JwtTokenProvider.java
@@ -1,14 +1,20 @@
 package com.server.token.Security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.server.token.domain.entity.User;
+import com.server.token.exception.CustomException;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
@@ -19,23 +25,35 @@ public class JwtTokenProvider {
     @Value("${security.jwt.token.secretKey}")
     private String secretKey;
 
-    public final static long TOKEN_VALIDATION_SECOND = 1000L * 60;  //1분을 accessToken 만료 기간으로 잡는다
-    public final static long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 120; //1시간을 refreshToken 만료 기간으로 잡는다.
+    public final static long TOKEN_VALIDATION_SECOND = 1000L * 60;  // 1분을 accessToken 만료 기간으로 잡는다
+    public final static long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 120; // 1시간을 refreshToken 만료 기간으로 잡는다.
 
     private final UserDetailsService userDetailsService; // 실제 데이터베이스에서 사용자 인증정보를 가져오는 userDetailsService 선언
 
-                    //@PostConstruct는 의존성 주입이 이루어진 후 초기화를 수행하는 메서드
-    @PostConstruct  // @PostConstruct가 붙은 메서드는 클래스가 service(로직을 탈 때? 로 생각 됨)를 수행하기 전에 발생한다. 이 메서드는 다른 리소스에서 호출되지 않는다해도 수행된다.
+    // @PostConstruct는 의존성 주입이 이루어진 후 초기화를 수행하는 메서드
+    @PostConstruct
+    // @PostConstruct가 붙은 메서드는 클래스가 service(로직을 탈 때? 로 생각 됨)를 수행하기 전에 발생한다. 이 메서드는 다른 리소스에서 호출되지 않는다해도 수행된다.
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes()); // secretKey를 Base64(암호화 알고리즘)으로 인코딩 시킴 (암호화 시킴)
     }
 
+    //accessToken 발급
+    public String generateToken(User user){
+        return createToken(user.getUserEmail(),user.getRoles(), TOKEN_VALIDATION_SECOND);
+    }
+
+    //refreshToken 발급
+    public String generateRefreshToken(User user){
+        return createToken(user.getUserEmail(),user.getRoles(), REFRESH_TOKEN_VALIDATION_SECOND);
+    }
+
     // JWT 토큰 생성
-    public String createToken(String userPk, List<String> roles) {
+    public String createToken(String userPk, List<String> roles, long expireTime) {
         Claims claims = Jwts.claims().setSubject(userPk); // JWT payload 에 저장되는 정보단위
         claims.put("roles", roles); // 정보는 key / value 쌍으로 저장된다.
+
         Date now = new Date();
-        Date validity = new Date(now.getTime() + TOKEN_VALIDATION_SECOND); // 유효시간 선언 [ 지금시간 + TOKEN_VALIDATION_SECOND 시간 ]
+        Date validity = new Date(now.getTime() + expireTime); // 유효시간 선언 [ 지금시간 + expireTime 시간 ]
         return Jwts.builder()
                 .setClaims(claims) // 정보 저장
                 .setIssuedAt(now) // 토큰 발행 시간 정보
@@ -44,5 +62,33 @@ public class JwtTokenProvider {
                 // signature 에 들어갈 secret값 세팅
                 .compact();
     }
+
+    public Authentication getAuthentication(String token) {
+        return null;
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // Request의 Header에서 token 값을 가져온다.
+    public String resolveToken(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new CustomException("Expired or invalid JWT token", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
