00f4f9adf023fa0af4640f8b5da0694b19e622cd
노경준 <developerjun0615@gmail.com>
2021-06-23 16:21:20 +0900

    [ADD] JwtTokenFilter 추가

---
+++ src/main/java/com/server/token/Security/JwtTokenFilter.java
@@ -0,0 +1,46 @@
+package com.server.token.Security;
+
+import com.server.token.exception.CustomException;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 토큰을 만들고 이 필터클래스(JwtAuthenticationFilter)에서 인증 작업 진행
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 헤더에서 JWT를 받아옴
+        String token = jwtTokenProvider.resolveToken(request);
+
+        try{
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                // SecurityContext 에 Authentication 객체를 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }catch(CustomException e){
+            SecurityContextHolder.clearContext();
+            response.sendError(e.getHttpStatus().value(), e.getMessage());
+            return ;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
