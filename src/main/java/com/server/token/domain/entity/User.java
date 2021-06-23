6ff0704c6493e1fe2e9d6e93774a82a3fac8739c
노경준 <developerjun0615@gmail.com>
2021-06-24 08:08:32 +0900

    [Update] UserDetails implements 적용

--- src/main/java/com/server/token/domain/entity/User.java
+++ src/main/java/com/server/token/domain/entity/User.java
@@ -3,16 +3,20 @@ package com.server.token.domain.entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @Entity
-@Builder
-public class User {
+public class User implements UserDetails {
     @Id @GeneratedValue
     private Long userIdx;
     private String userEmail;
@@ -25,5 +29,40 @@ public class User {
     public void update(String userEmail,String userPw){
         this.userEmail = userEmail;
         this.userPw = userPw;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
