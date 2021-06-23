083ba14b46ab9eae1f12a736ff9964f155974f31
노경준 <developerjun0615@gmail.com>
2021-06-23 16:20:15 +0900

    [Update] User roles 추가

--- src/main/java/com/server/token/domain/entity/User.java
+++ src/main/java/com/server/token/domain/entity/User.java
@@ -1,20 +1,26 @@
 package com.server.token.domain.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
+@Builder
 public class User {
     @Id @GeneratedValue
     private Long userIdx;
     private String userEmail;
     private String userPw;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
 
     public void update(String userEmail,String userPw){
         this.userEmail = userEmail;
