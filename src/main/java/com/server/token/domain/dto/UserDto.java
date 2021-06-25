5355fc542744c87ce1ac62aa3166dcfce60743a1
노경준 <developerjun0615@gmail.com>
2021-06-25 10:52:15 +0900

    Update UserDto.java

--- src/main/java/com/server/token/domain/dto/UserDto.java
+++ src/main/java/com/server/token/domain/dto/UserDto.java
@@ -1,12 +1,27 @@
 package com.server.token.domain.dto;
 
+import com.server.token.domain.entity.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
+@Builder
 public class UserDto {
     private String userEmail;
     private String userPw;
+    public UserDto(String userEmail,String userPw){
+        this.userEmail = userEmail;
+        this.userPw = userPw;
+    }
+
+    public User toEntity(){
+        return User.builder()
+                .userEmail(this.getUserEmail())
+                .userPw(this.getUserPw())
+                .build();
+    }
 }
