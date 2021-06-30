52f244f9c2e79d4fe610c1fce4ccd2db67aa59b4
노경준 <developerjun0615@gmail.com>
2021-06-30 15:18:22 +0900

    Update RfreshTokenServiceImpl

--- src/main/java/com/server/token/service/Impl/RefreshTokenServiceImpl.java
+++ src/main/java/com/server/token/service/Impl/RefreshTokenServiceImpl.java
@@ -25,7 +25,31 @@ public class RefreshTokenServiceImpl implements RefreshTokenService {
         UserDto userDto = new UserDto();
         List<Role> roles = userDto.toEntity().getRoles();
 
+        String accessToken = jwtTokenProvider.resolveToken(request);
+        String refreshToken = jwtTokenProvider.resolveRefreshToken(request);
+        String newAccessToken = null;
+        String newRefreshToken = null;
+        String nickname = jwtTokenProvider.getUsername(accessToken);
+
         Map<String,String> map = new HashMap<>();
+
+        if(redisUtil.getData(nickname) == null){ // redisUtil 로 nickname을 보내주어 토큰이 유효한지 검증하는 조건문
+            map.put("message","로그아웃된 토큰입니다.");
+        }
+
+        // redis에 있는 refreshToken과 reqeust 요청으로 들어온 refreshToken을 비교하여 같고,
+        // JwtTokenProvider클래스의 validateToken 메소드를 이용하여 토큰이 유효한 조건이 맞다면
+        // 아래 코드 실행
+        if(redisUtil.getData(nickname).equals(refreshToken) && jwtTokenProvider.validateToken(refreshToken)){
+            redisUtil.deleteData(nickname); // redis에 있는 refreshToken를 초기화
+            newAccessToken = jwtTokenProvider.createToken(nickname, roles);
+            newRefreshToken = jwtTokenProvider.createRefreshToken();
+            redisUtil.setDataExpire(nickname,newRefreshToken,jwtTokenProvider.REFRESH_TOKEN_VALIDATION_SECOND);
+            map.put("nickname",nickname);
+            map.put("newAccessToken",newAccessToken); // newAccessToken 반환
+            map.put("newRefreshToken",newRefreshToken); // newRefreshToken 반환
+            return map;
+        }
 
         return map;
     }
