d800b76fbfae143c9433bc8d3ae0053f376c8d17
노경준 <developerjun0615@gmail.com>
2021-07-01 15:41:16 +0900

    Update CRUD 코드 삭제 및 주석 추가

--- src/main/java/com/server/token/service/Impl/UserServiceImpl.java
+++ src/main/java/com/server/token/service/Impl/UserServiceImpl.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,65 +24,38 @@ public class UserServiceImpl implements UserService {
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisUtil redisUtil;
 
-    //signup
+    // 회원가입
     @Override
-    public String signup(UserDto userDto) {
+    public User signup(UserDto userDto) {
         if(userRepository.findByUserEmail(userDto.getUserEmail()) != null){
             throw new UserAlreadyExistsException("해당 아이디는 이미 존재합니다.");
         }
         userDto.setUserPw(passwordEncoder.encode(userDto.getUserPw())); // 패스워드를 암호화하여 저장
-        userRepository.save(userDto.toEntity());
-        return "성공";
+        User userInfo = userRepository.save(userDto.toEntity());
+        return userInfo;
     }
 
-    //signin
+    // 로그인
     @Override
     public Map<String, String> signin(LoginDto loginDto) {
-        User findUser = userRepository.findByUserEmail(loginDto.getUserEmail());
-        if(findUser == null) throw new UserNotFoundException("해당 유저를 찾을 수 없습니다.");  // 유저가 존재하는지 확인
+        User findUser = userRepository.findByUserEmail(loginDto.getUserEmail()); // loginDto 에서 유저의 이메일을 가져와서 findUser변수에 저장
+        if(findUser == null) throw new UserNotFoundException("해당 유저를 찾을 수 없습니다.");  // findUser의 값이 null이라면 UserNotFoundException메소드 실행
 
         boolean passwordCheck = passwordEncoder.matches(loginDto.getUserPw(), findUser.getUserPw()); // 인코딩 후 dto에 저장된 pw값과 db에 저장된 pw를 비교 후 같으면 true 다르면 false 반환
-        if(!passwordCheck) throw new UserNotFoundException(); // passwordCheck 후 true가 아니라면 Exception, false면 무시
+        if(!passwordCheck) throw new UserNotFoundException("비밀번호가 올바르지 않습니다."); // passwordCheck 후 true가 아니라면 Exception, false면 무시
 
-        Map<String,String> map = new HashMap<>();
-        String accessToken = jwtTokenProvider.createToken(loginDto.getUserEmail(),loginDto.toEntity().getRoles());
-        String refreshToken = jwtTokenProvider.createRefreshToken();
+        Map<String,String> map = new HashMap<>(); // 값을 반환할 KEY : VALUE 를 담을 수있는 Map 생성
+        String accessToken = jwtTokenProvider.createToken(loginDto.getUserEmail(),loginDto.toEntity().getRoles()); // accessToken 생성
+        String refreshToken = jwtTokenProvider.createRefreshToken(); // refreshToken 생성
 
-        redisUtil.deleteData(loginDto.getUserEmail());
+        redisUtil.deleteData(loginDto.getUserEmail()); // redis에 값을 삽입하기 전 해당 아이디의 refreshToken 삭제
         redisUtil.setDataExpire(findUser.getUserEmail(), refreshToken, JwtTokenProvider.REFRESH_TOKEN_VALIDATION_SECOND); // redis에 값을 삽입하기위해 key,value,만료시간 설정
 
+        // response를 하기위한 map에 userEmail,accessToken,refreshToken put
         map.put("userEmail", loginDto.getUserEmail());
         map.put("accessToken","Bearer " + accessToken);
         map.put("refreshToken","Bearer " + refreshToken);
 
         return map;
-    }
-
-    @Override
-    public User read(Long idx) {
-        return userRepository.findById(idx).orElseThrow(UserNotFoundException :: new);
-    }
-
-    @Transactional
-    @Override
-    public void update(Long idx, UserDto userDto) {
-        User user = read(idx);
-        user.update(userDto.getUserEmail(), userDto.getUserPw());
-    }
-
-    @Override
-    public void delete(Long idx) {
-        userRepository.deleteById(idx);
-    }
-
-
-    public String 새로운_토큰_발급(String re){
-
-        //A, R => 헤더
-        //A 토큰 안에있는 사용자 검증
-        // Redis안에 있는 R랑 헤더로 들어온 R ==
-        // 새롭게 A발급
-        // 틀리면 에러 처리
-        return "";
     }
 }
