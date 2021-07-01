af3d4444f321781069ad1fcdf5698dbb4788dae9
노경준 <developerjun0615@gmail.com>
2021-07-01 15:41:01 +0900

    Update UserService 로직 일부 삭제

--- src/main/java/com/server/token/service/UserService.java
+++ src/main/java/com/server/token/service/UserService.java
@@ -5,12 +5,8 @@ import com.server.token.domain.dto.UserDto;
 import com.server.token.domain.entity.User;
 
 import java.util.Map;
-import java.util.Optional;
 
 public interface UserService {
-    String signup(UserDto userDto);
+    User signup(UserDto userDto);
     Map<String, String> signin(LoginDto loginDto);
-    User read(Long idx);
-    void update(Long idx, UserDto userDto);
-    void delete(Long idx);
 }
