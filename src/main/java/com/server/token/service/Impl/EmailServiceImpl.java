ac5e9478bd29a99262217262e461072b64341560
노경준 <developerjun0615@gmail.com>
2021-07-03 13:43:45 +0900

    Update 메소드 수정

--- src/main/java/com/server/token/service/Impl/EmailServiceImpl.java
+++ src/main/java/com/server/token/service/Impl/EmailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.server.token.service.Impl;
 
+import com.server.token.domain.dto.UserEmailDto;
 import com.server.token.service.EmailService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +14,11 @@ public class EmailServiceImpl implements EmailService {
     private final JavaMailSender javaMailSender;
 
     @Override
-    public void sandEmail(String userEmail, String title, String text) {
+    public void sendEmail(String userEmail, String title, String key) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(userEmail);
         message.setSubject(title);
-        message.setText(text);
+        message.setText("Spring boot에서 보낸 인증 키 : " + key);
         javaMailSender.send(message);
     }
 }
