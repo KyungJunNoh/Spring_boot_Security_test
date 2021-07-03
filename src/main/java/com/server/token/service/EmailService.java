45a88682e33921014fa5f85b7b5bd257d572bebc
노경준 <developerjun0615@gmail.com>
2021-07-03 13:43:30 +0900

    Update EmailService 오타 수정

--- src/main/java/com/server/token/service/EmailService.java
+++ src/main/java/com/server/token/service/EmailService.java
@@ -1,5 +1,5 @@
 package com.server.token.service;
 
 public interface EmailService {
-    void sandEmail(String userEmail,String title,String text);
+    void sendEmail(String userEmail,String title,String key);
 }
