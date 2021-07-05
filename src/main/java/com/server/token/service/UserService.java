7a4d67446bc44dea4c5f6b82e950f72a4a74c758
노경준 <developerjun0615@gmail.com>
2021-07-05 13:46:51 +0900

    Update changePassword 메소드 추가

--- src/main/java/com/server/token/service/UserService.java
+++ src/main/java/com/server/token/service/UserService.java
@@ -1,11 +1,9 @@
 package com.server.token.service;
 
-import com.server.token.domain.dto.FindPasswordDto;
-import com.server.token.domain.dto.LoginDto;
-import com.server.token.domain.dto.UserDto;
-import com.server.token.domain.dto.UserEmailDto;
+import com.server.token.domain.dto.*;
 import com.server.token.domain.entity.User;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 public interface UserService {
@@ -13,4 +11,5 @@ public interface UserService {
     Map<String, String> signin(LoginDto loginDto);
     String emailAuthentication(UserEmailDto userEmailDto);
     String findPassword(FindPasswordDto findPasswordDto);
+    String changePassword(HttpServletRequest httpServletRequest, ChangePasswordRequestDto changePasswordRequestDto);
 }
