486d75951181f51a3ab5ac3b0e8b50eed1306dde
노경준 <developerjun0615@gmail.com>
2021-07-06 17:41:11 +0900

    Create NoticeBoardController.java

---
+++ src/main/java/com/server/token/controller/NoticeBoardController.java
@@ -0,0 +1,10 @@
+package com.server.token.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class NoticeBoardController {
+
+}
