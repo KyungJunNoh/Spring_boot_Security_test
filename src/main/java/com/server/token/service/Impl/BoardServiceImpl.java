1518516df6fc7098f7105c41455790fc3c53dcff
노경준 <developerjun0615@gmail.com>
2021-07-10 15:13:20 +0900

    Update BoardServiceImpl.java

--- src/main/java/com/server/token/service/Impl/BoardServiceImpl.java
+++ src/main/java/com/server/token/service/Impl/BoardServiceImpl.java
@@ -5,22 +5,29 @@ import com.server.token.domain.dto.BoardUpdateRequestDto;
 import com.server.token.domain.entity.Board;
 import com.server.token.exception.UserNotFoundException;
 import com.server.token.repository.BoardRepository;
+import com.server.token.repository.UserRepository;
+import com.server.token.security.JwtTokenProvider;
 import com.server.token.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
     private final BoardRepository boardRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
 
-    @Transactional
     @Override
-    public Long create(BoardCreateRequestDto boardCreateRequestDto) {
-        return boardRepository.save(boardCreateRequestDto.toEntity()).getIdx();
+    public void create(BoardCreateRequestDto boardCreateRequestDto, HttpServletRequest httpServletRequest) {
+        String token = jwtTokenProvider.resolveToken(httpServletRequest);
+        String name = jwtTokenProvider.getUsername(token);
+        boardCreateRequestDto.setUser(userRepository.findByUserEmail(name));
+        boardRepository.save(boardCreateRequestDto.toEntity());
     }
 
     @Transactional
