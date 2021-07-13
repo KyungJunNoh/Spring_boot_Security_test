bd003f5a2433b4b471cdb1f74e0df25a7cb0ca60
노경준 <developerjun0615@gmail.com>
2021-07-13 20:24:55 +0900

    Update BoardCreateRequestDto.java

--- src/main/java/com/server/token/domain/dto/BoardCreateRequestDto.java
+++ src/main/java/com/server/token/domain/dto/BoardCreateRequestDto.java
@@ -5,20 +5,14 @@ import com.server.token.domain.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
-@NoArgsConstructor
 public class BoardCreateRequestDto {
     private User user;
     private String title;
     private String content;
-
-    @Builder
-    public BoardCreateRequestDto(User user,String title, String content){
-        this.user = user;
-        this.title = title;
-        this.content = content;
-    }
 
     public Board toEntity(){
         return Board.builder()
